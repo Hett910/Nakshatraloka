0fa8f6f7d2fc8211b3c964ed2caa29441077dc3f
Hett910 <hettsoni910@gmail.com>
2025-08-22 20:50:29 +0530

    Add User APIs (saveUser, login, updatePassword)

--- src/router/Category/Category.route.js
+++ src/router/Category/Category.route.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { Catogary } = require('../../controller/Category/Category.Controller');
 const { SaveCatogaryValidation } = require('../../utils/Validation');
+const auth = require('../../middleware/Auth');
 
-router.post('/saveCatogary', SaveCatogaryValidation, Catogary.saveCategory);
+router.post('/saveCatogary', [auth, SaveCatogaryValidation], Catogary.saveCategory);
 router.post('/getAllCatogary', Catogary.getAllCategory);
 router.post('/getCatogaryById/:id', Catogary.getCatogaryById);
-router.post('/deleteCatogary/:id', Catogary.deleteCategory);
+router.post('/deleteCatogary/:id', auth, Catogary.deleteCategory);
 
 module.exports = router;
--- src/utils/Validation.js
+++ src/utils/Validation.js
@@ -8,6 +8,31 @@ const SaveCatogaryValidation = [
     body('isShown').isBoolean().withMessage('isShown must be boolean')
 ];
 
+const SaveUserValidation = [
+
+    body('fullname')
+        .trim()
+        .notEmpty().withMessage('Full Name is required')
+        .isLength({ min: 3 }).withMessage('Full Name must be at least 3 characters long')
+        .escape(),
+
+    body('email')
+        .trim()
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Invalid email format')
+        .normalizeEmail(),
+
+    body('phone')
+        .trim()
+        .notEmpty().withMessage('Phone number is required')
+        .isMobilePhone().withMessage('Invalid phone number'),
+
+    body('password_hash')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+]
+
 module.exports = {
-        SaveCatogaryValidation
+    SaveCatogaryValidation,
+    SaveUserValidation
 }
