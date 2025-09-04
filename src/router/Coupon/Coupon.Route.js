678053a3b297aa117737eed89c9bd1052a293c29
HetSoni_Nakshatraloka_UI <hettsoni910@gmail.com>
2025-09-05 01:45:32 +0530

    add comment in coupon

--- src/router/Coupon/Coupon.Route.js
+++ src/router/Coupon/Coupon.Route.js
@@ -3,6 +3,7 @@ const { Coupon } = require('../../controller/Coupon/Coupon.Controller');
 const router = express.Router();
 const auth = require('../../middleware/Auth');
 
+// Add
 router.post('/saveCoupon', auth ,Coupon.SaveCoupon);
 router.post('/getAllCoupons', auth ,Coupon.getAllCoupons);
 router.post('/deleteCoupon/:id', auth ,Coupon.deleteCoupon);
